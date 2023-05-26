5df49054bb74a41602c531896a4f7c1a48c30c7a
Katherine Clark <60265279+KClarkDev@users.noreply.github.com>
2023-05-26 14:40:11 -0400

    Fixed form styling with legend/field set elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,9 @@
 var characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '#', '$', '%', '&', '(', ')', '*', '+', '_'];
 
 // TODO: add validation
-var passwordLength = prompt("How many characters do you want in the password? (must be between 8 and 128 characters)");
+// var passwordLength = prompt("How many characters do you want in the password? (must be between 8 and 128 characters)");
+
+var passwordLength = document.querySelector("#passwordLength");
 
 
 function getRandomChar(arr) {
