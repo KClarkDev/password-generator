b3a7afd9c502f65de2044c5beabad0f542719131
Katherine Clark <60265279+KClarkDev@users.noreply.github.com>
2023-05-28 10:52:40 -0400

    Adding comments to html and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,48 +2,22 @@
 //////////////////GLOBAL VARIABLES//////////////////
 ////////////////////////////////////////////////////
 
-// array of valid characters to be used in the password
-// var characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '#', '$', '%', '&', '(', ')', '*', '+', '_'];
-
 var passwordLengthInput = document.querySelector("#passwordLength");
 
 var passwordLength = passwordLengthInput.value;
-
-
-
-
 
 /////////////////////////////////////////////
 //////////////////FUNCTIONS//////////////////
 /////////////////////////////////////////////
 
-// function getRandomChar(arr) {
-
-//   // get random index value
-//   var randomIndex = Math.floor(Math.random() * arr.length);
-
-//   // get random item
-//   return arr[randomIndex];
-// }
-
-// // calls the getRandomChar() function and returns a string of random characters that has the length of the passwordLength variable
-// function generatePassword(passLength) {
-//   var userPassword = '';
-
-//   for (var i=0; i < passLength; i++) {
-//     var character = getRandomChar(characters); //references global array
-//     userPassword = userPassword.concat(character);
-//   }
-//   return userPassword;
-// }
-
 function generatePassword() {
-  var passwordForm = document.getElementById("passwordForm");
-  var passwordLength = parseInt(passwordForm.elements.passwordLength.value);
-  var charTypeElements = passwordForm.elements.charType;
+  var passwordForm = document.getElementById("passwordForm"); // Get access to the form element in the HTML
+  var passwordLength = parseInt(passwordForm.elements.passwordLength.value); // Access the password length input value and convert from text to integer
+  var charTypeElements = passwordForm.elements.charType; // Get access to all the elements in the HTML with "name" equal to "charType". This returns a NodeList (represents a collection of nodes (elements) in the document order. You can access individual elements from the collection using their index, similar to an array.)
 
   var selectedCharTypes = []; // Array to store selected character types
 
+  // Iterate through the charType nodeList. If the value is checked, add the charType to the selectedCharTypes array.
   for (var i = 0; i < charTypeElements.length; i++) {
     if (charTypeElements[i].checked) {
       selectedCharTypes.push(charTypeElements[i].value);
@@ -58,7 +32,8 @@ function generatePassword() {
     Special: "!@#$%^&*()"
   };
 
-  // Combine selected character sets into a single string
+  // Combines the character sets based on the selected character types (selectedCharTypes) by reducing them into a single string using the reduce method.
+  // NOTE FOR STUDY: This is a function expression - when a function is assigned to a variable or a property of an object
   var characters = selectedCharTypes.reduce(function(acc, charType) {
     return acc + characterSets[charType];
   }, "");
@@ -74,16 +49,7 @@ function generatePassword() {
   passwordText.value = generatedPassword;
 }
 
-// // Write password to the #password input
-// function writePassword() {
-//   var passwordLength = passwordLengthInput.value; // Get the value of the input field
-//   var password = generatePassword(parseInt(passwordLength)); // Convert the value to an integer
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-// }
-
-// Function to copy password to clipboard
+// Copies password to clipboard
 function copyPasswordToClipboard() {
   var passwordText = document.querySelector("#password");
   passwordText.select();
@@ -135,6 +101,44 @@ generateBtn.addEventListener("click", generatePassword);
 var copyButton = document.querySelector("#copyButton");
 // Add event listener to copy button
 copyButton.addEventListener("click", copyPasswordToClipboard);
+
+
+///////////////////////////////////////////////////
+//////////////////////ARCHIVE//////////////////////
+///////////////////////////////////////////////////
+
+// This code was removed from the final script since it was combined into another function. Keeping it in the file for notes/reference since this was an educational exercise.
+
+// function getRandomChar(arr) {
+
+//   // get random index value
+//   var randomIndex = Math.floor(Math.random() * arr.length);
+
+//   // get random item
+//   return arr[randomIndex];
+// }
+
+// // calls the getRandomChar() function and returns a string of random characters that has the length of the passwordLength variable
+// function generatePassword(passLength) {
+//   var userPassword = '';
+
+//   for (var i=0; i < passLength; i++) {
+//     var character = getRandomChar(characters); //references global array
+//     userPassword = userPassword.concat(character);
+//   }
+//   return userPassword;
+// }
+
+// // Write password to the #password input
+// function writePassword() {
+//   var passwordLength = passwordLengthInput.value; // Get the value of the input field
+//   var password = generatePassword(parseInt(passwordLength)); // Convert the value to an integer
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+// }
+
+
 
 
 
